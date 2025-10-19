6ebebc68b2870561ceb145ae01e74931ca0ad29d
MARGUIN Benjamin <marguinbenjamin@gmail.com>
2025-10-19 19:43:29 +0200

    BlackScholesPricer Class

--- Options_pricing/Options_pricing/PutOption.cpp
+++ Options_pricing/Options_pricing/PutOption.cpp
@@ -1,10 +1,12 @@
 #include "PutOption.h"
 
+// Constructor: initializes expiry and strike
 PutOption::PutOption(double expiry, double strike)
 	: EuropeanVanillaOption(expiry, strike)
 {
 }
 
+// Payoff function: max(K - S, 0)
 double PutOption::payoff(double spot) const
 {
 	if (spot < getStrike())
@@ -13,8 +15,8 @@ double PutOption::payoff(double spot) const
 		return 0.0;
 }
 
+// Returns option type (put)
 EuropeanVanillaOption::optionType PutOption::GetOptionType() const
 {
 	return EuropeanVanillaOption::optionType::put;
 }
-
